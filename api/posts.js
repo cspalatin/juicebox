5be6b3f69e8d56226bd13d44beeff2be823962d4
Christopher Spalatin <christopherspalatin@Christophers-iMac.local>
2021-04-25 14:04:48 -0500

    part 3 OH with Angie

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const postsRouter = express.Router();
 
-const { getAllPosts } = require("../db");
+const { getAllPosts, createPost } = require("../db");
+
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+
+    const post = await createPost(postData);
+
+    if (post) {
+      res.send({ post });
+    } else {
+      next({ name: "post creation error", message: "error with post" });
+    }
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.get("/", async (req, res) => {
   const posts = await getAllPosts();
