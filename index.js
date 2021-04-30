d3e52b73ef0f5fe8c72a1c18f2eba8c72d9b437e
Christopher Spalatin <christopherspalatin@Christophers-iMac.local>
2021-04-29 21:27:58 -0500

    additional heroku stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 
 const express = require("express");
 const server = express();
@@ -27,4 +27,20 @@ server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
+});
+
+server.get("/background/:color", (req, res, next) => {
+  res.send(`
+    <body style="background: ${req.params.color};">
+      <h1>Hello World</h1>
+    </body>
+  `);
+});
+
+server.get("/add/:first/to/:second", (req, res, next) => {
+  res.send(
+    `<h1>${req.params.first} + ${req.params.second} = ${
+      Number(req.params.first) + Number(req.params.second)
+    }</h1>`
+  );
 });
